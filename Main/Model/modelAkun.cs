ca3c28db38f63e46db317cab8484f59678ed58b5
Dafa24 <53173575+Dafa24@users.noreply.github.com>
2024-06-12 18:24:23 +0700

    Adding Model View Controller

---
+++ Main/Model/modelAkun.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Model
+{
+    internal class modelAkun
+    {
+        public class Akun
+        {
+            public int Id { get; set; }
+
+            public string Username { get; set; }
+
+            public string Nama { get; set; }
+
+            public string Password {  get; set; }
+
+            public string Role { get; set; }
+
+            public Akun(int Id,string Username,string Password,string Nama, string Role)
+            {
+                this.Id = Id;
+                this.Username = Username;
+                this.Password = Password;
+                this.Nama = Nama;
+                this.Role = Role;
+            }
+        }
+    }
+
+}
+
---
+++ View/Homepage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class Homepage : Form
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lihatTabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
