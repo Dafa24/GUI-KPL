6a256892ea48c1d57e8a7d6642113996343c4e34
Dafa24 <53173575+Dafa24@users.noreply.github.com>
2024-06-12 23:25:13 +0700

    Adding View Table and Fixing UI of Homepage and some error

--- View/obj/Debug/net8.0-windows/View.AssemblyInfo.cs
+++ View/obj/Debug/net8.0-windows/View.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("View")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b8eabb021b7d331f06a173e422081e7f50e8153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8110bc7a5bcc3336bf76d483b2d60f7e4f6e94ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("View")]
 [assembly: System.Reflection.AssemblyTitleAttribute("View")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
